0df09914172dd431f0edd36fb1027f21bed3e46e
YanIgnacio <yandasilva3@gmail.com>
2023-09-26 15:46:37 -0300

    0.0.1 inicio

---
+++ os_ti/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class IluminacaoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'os_ti'
+    verbose_name = 'Solicitação de Serviços TI'
